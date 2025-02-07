5c2976bae34bed91025f864e0110ebfdb5a76e16
MireyaRuiz01 <148653063+MireyaRuiz01@users.noreply.github.com>
2025-02-07 12:53:36 -0600

    initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+
+
+const buttonNo = document.querySelector('#no')
+const buttonYes = document.querySelector('#yes')
+
+let fontSize = 2
+
+let messages = [
+  'Estas seguro amor?',
+  'Ni modo ',
+  'Ya toca el otro botón jaja',
+  
+]
+
+buttonNo.addEventListener('click', () => {
+  fontSize = fontSize + .5
+  buttonYes.style.fontSize = `${fontSize}rem`
+
+  const indexRandom = Math.floor(Math.random() * messages.length)
+
+  buttonNo.textContent = messages[indexRandom]
+})
+
+buttonYes.addEventListener('click', () => {
+  document.querySelector('#message').style.display = 'flex'
+})
